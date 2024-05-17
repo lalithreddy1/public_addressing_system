3f9142cb10e6e60361a1bd5ebbc32f6b0e255145
lalithreddy1 <98268843+lalithreddy1@users.noreply.github.com>
2024-05-17 13:23:28 +0530

    File_save.js

--- 1.js
+++ 1.js
@@ -1,10 +1,26 @@
 const express = require("express");
 const path = require("path");
 app = express();
+
+
+const multer = require('multer');
+const {exec}= require("child_process");
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+      cb(null, 'uploads/');// Directory where uploaded files will be saved
+  },
+  filename: function (req, file, cb) {    
+      cb(null, +"1.mp3");// Unique filename
+  }
+});
+const upload = multer({ storage: storage });
 app.set('view engine', 'ejs');
 app.use("/js", express.static("js"));
 app.use("/images", express.static("images"));
 app.use(express.static("public"));
+app.post("/upload",upload.single("mp3file"),(req,res)=>{
+  res.send('File uploaded successfully');
+})
 app.get("/", (req, res) => {
   res.sendFile(path.resolve(__dirname + "/broadcast.html"));
 });
